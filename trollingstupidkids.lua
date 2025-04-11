3da2815b364f0615d4eb25bf7cd5a586a5a3fd0c
agg-real <agg603450@gmail.com>
2025-04-11 22:37:51 +0300

    Update trollingstupidkids.lua

--- trollingstupidkids.lua
+++ trollingstupidkids.lua
@@ -1,9 +1,12 @@
 local chr = game.Players.LocalPlayer.Character
-for _,part in ipairs(stairs:GetChildren()) do
+for _,part in ipairs(workspace.Stairs:GetChildren()) do
 if part:IsA("BasePart") then
 part.CanCollide = false
 part.CFrame = chr:WaitForChild("HumanoidRootPart")
 part.Size = Vector3.new(5,12,1.45)
   end
 end
-
+local bambam = Instance.new("BodyThrust")
+bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+bambam.Force = Vector3.new(power,0,900000)
+bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
