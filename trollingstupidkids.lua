4e7ac1b00f2b4f210e6cb66960149735b62c3f5e
agg-real <agg603450@gmail.com>
2025-04-11 22:57:28 +0300

    Update trollingstupidkids.lua

--- trollingstupidkids.lua
+++ trollingstupidkids.lua
@@ -11,9 +11,3 @@ local bambam = Instance.new("BodyThrust")
 bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
 bambam.Force = Vector3.new(power,0,129990)
 bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
-local noclip = true
-game:GetService("RunService").RenderStepped:Connect(function()
-   if noclip then
-    game.Players.LocalPlayer.Character.Humanoid:SetState(11)
-   end
-end
