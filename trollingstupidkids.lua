84f32e5e48ab25d02fdb8fc5fca9406daa22af0a
agg-real <agg603450@gmail.com>
2025-04-11 22:46:20 +0300

    Update trollingstupidkids.lua

--- trollingstupidkids.lua
+++ trollingstupidkids.lua
@@ -7,18 +7,15 @@ part.CFrame = chr:WaitForChild("HumanoidRootPart").CFrame
 part.Size = Vector3.new(5,12,1.45)
   end
 end
-local noclip = true -- Gets if you want the noclip
-char = game.Players.LocalPlayer.Character -- Gets your player
-while true do -- Make sure someone is in game
-if noclip == true then
-for _,v in pairs(char:children()) do
-pcall(function()
-if v.className == "Part" then
-v.CanCollide = false
-end
-end)
+
+while task.wait() do
+for _,part in ipairs(stairs:GetChildren()) do
+if part:IsA("BasePart") then
+part.CanCollide = false
+  end
 end
 end
+
 local bambam = Instance.new("BodyThrust")
 bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
 bambam.Force = Vector3.new(power,0,900000)
