e2d7ffe2ff812aa49b1c4298e383b143c288b8f7
agg-real <agg603450@gmail.com>
2025-04-11 23:14:18 +0300

    Update trollingstupidkids.lua

--- trollingstupidkids.lua
+++ trollingstupidkids.lua
@@ -1,9 +1,35 @@
-local chr = game.Players.LocalPlayer.Character
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))()
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local RunService = game:GetService("RunService")
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
+local HttpService = game:GetService("HttpService")
+
+local Window = OrionLib:MakeWindow({Name = "Skill issue obby hub", HidePremium = false, SaveConfig = true})
+local Tab = Window:MakeTab({Name = "Главная", Icon = "rbxassetid://89991291117147"})
+local flightEnabled = false
+Tab:AddToggle({
+    Name = "Включить функцию",
+    Default = flightEnabled,
+    Callback = function(value)
+        flightEnabled = value
+    end
+})
+
+task.spawn(function()
+    while task.wait() do
+if flightEnabled then
+ local chr = game.Players.LocalPlayer.Character
 local stairs = workspace:FindFirstChild("Stairs")
-for _,part in ipairs(stairs:GetChildren()) do
+for _,part in pairs(stairs:GetChildren()) do
 if part:IsA("BasePart") then
 part.CanCollide = false
 part.CFrame = chr:WaitForChild("HumanoidRootPart").CFrame
 part.Size = Vector3.new(5,12,1.45)
   end
 end
+end)
+
+OrionLib:Init()
