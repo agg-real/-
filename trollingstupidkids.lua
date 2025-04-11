28805cfb6353df7c4ec1c8d2371ef1d9f73b4cb7
agg-real <agg603450@gmail.com>
2025-04-11 22:42:17 +0300

    Update trollingstupidkids.lua

--- trollingstupidkids.lua
+++ trollingstupidkids.lua
@@ -1,8 +1,9 @@
 local chr = game.Players.LocalPlayer.Character
-for _,part in ipairs(workspace.Stairs:GetChildren()) do
+local stairs = workspace:FindFirstChild("Stairs")
+for _,part in ipairs(stairs:GetChildren()) do
 if part:IsA("BasePart") then
 part.CanCollide = false
-part.CFrame = chr:WaitForChild("HumanoidRootPart")
+part.CFrame = chr:WaitForChild("HumanoidRootPart").CFrame
 part.Size = Vector3.new(5,12,1.45)
   end
 end
