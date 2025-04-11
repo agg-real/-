447d85d94745a227e4ec7e85f90b47f085916c4c
agg-real <agg603450@gmail.com>
2025-04-11 22:35:45 +0300

    Update trollingstupidkids.lua

--- trollingstupidkids.lua
+++ trollingstupidkids.lua
@@ -6,7 +6,4 @@ part.CFrame = chr:WaitForChild("HumanoidRootPart")
 part.Size = Vector3.new(5,12,1.45)
   end
 end
-local bambam = Instance.new("BodyThrust")
-bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
-bambam.Force = Vector3.new(power,0,126562)
-bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+
